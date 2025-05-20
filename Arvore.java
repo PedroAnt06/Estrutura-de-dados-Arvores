0be380cefa645124cbaaf5e316e25b8f22a4eae5
Pedro Antonio <pedroantoniolopesdearruda6@gmail.com>
2025-05-20 19:37:58 -0300

    Estou adicionando o metodo de contarNos

--- Arvore.java
+++ Arvore.java
@@ -1,4 +1,9 @@
 public class Arvore {
 
     No raiz;
+
+    public int contarNos(No node) {
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
 }
